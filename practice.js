006480c2b7404be2b3b02c04c8af6042a995cf9a
quanle28197 <86665723+quanle28197@users.noreply.github.com>
2022-01-04 15:10:43 +0700

    add

--- practice.js
+++ practice.js
@@ -1,10 +1,30 @@
-let inputPhysics = prompt('Enter the Physics');
-let inputChemis = prompt('Enter the Chemis');
-let inputBiology = prompt('Enter the Biology');
-let Physics = parseInt(inputPhysics);
-let Chemis = parseInt(inputChemis);
-let Biology = parseInt(inputBiology);
-let avarage = (Physics + Chemis + Biology) / 3;
-let total = Physics + Chemis + Biology;
-document.write('Average = ' + avarage);
-document.write('Total =' + total);
+// let inputPhysics = prompt('Enter the Physics');
+// let inputChemis = prompt('Enter the Chemis');
+// let inputBiology = prompt('Enter the Biology');
+// let Physics = parseInt(inputPhysics);
+// let Chemis = parseInt(inputChemis);
+// let Biology = parseInt(inputBiology);
+// let avarage = (Physics + Chemis + Biology) / 3;
+// let total = Physics + Chemis + Biology;
+// document.write('Average = ' + avarage);
+// document.write('<br/>');
+// document.write('Total =' + total);
+
+// Bai 2
+// let inputCelsius = prompt('Enter the Celsius');
+// let Celsius = parseInt(inputCelsius);
+// let fahrenheit = Celsius * 9/5 + 32;
+// document.write('Fahrenheit = ' + fahrenheit);
+
+//Bai 3:
+// let inputCircleRadius = prompt('Enter the Circle Radius');
+// let R = parseInt(inputCircleRadius);
+// let acarge = 3.14 * (R * 2 );
+// document.write('Acarge =' + acarge);
+
+// Bai 4
+
+let inputCircleRadius = prompt('Enter the Circle Radius');
+let R = parseInt(inputCircleRadius);
+let perimeter = 2 * R * 3.14;
+document.write('Perimeter = ' + perimeter);
